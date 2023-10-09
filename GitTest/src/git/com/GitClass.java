723b146b274758df27176581946c62ebca0fe4cf
meghabawane <meghabawane95@gmail.com>
2023-10-09 15:55:35 +0530

    first p

---
+++ GitTest/src/git/com/GitClass.java
@@ -0,0 +1,11 @@
+package git.com;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test for push program");
+
+	}
+
+}
